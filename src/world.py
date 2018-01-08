e2e6257c256e63490802cdb10938893ff4ef8b9a
Fabio Frazao <fsfrazao@gmail.com>
2018-01-08 13:46:47 -0330

    autopep8

--- src/world.py
+++ src/world.py
@@ -1,6 +1,7 @@
 import numpy as np
-from math import floor,atan2,degrees,sin,cos,radians, isnan
+from math import floor, atan2, degrees, sin, cos, radians, isnan
 from operator import methodcaller
+
 
 class World(object):
 
@@ -26,20 +27,20 @@ class World(object):
 
     def __repr__(self):
         return "World object"
-    def __init__(self,topology,steps_in_day=24):
-        self.topology=topology
-        self.day=1
-        self.month=1
-        self.year=1
-        self.step=0
-        self.steps_in_day=steps_in_day
 
+    def __init__(self, topology, steps_in_day=24):
+        self.topology = topology
+        self.day = 1
+        self.month = 1
+        self.year = 1
+        self.step = 0
+        self.steps_in_day = steps_in_day
 
     def active_agents(self):
-    	""" Return a list of active agents. """
-    	pass
+        """ Return a list of active agents. """
+        pass
 
-    def run_simulation(self,n):
+    def run_simulation(self, n):
         """ Run the schedule n times.
 
             Note: actions to be performed before or after all the schedule could be included here. An example would be writing model outputs to a file every 10 steps.
@@ -54,14 +55,12 @@ class World(object):
             self.run_schedule()
 
     def run_schedule(self):
-    	""" This method is supposed to be overridden.
-    	It should include the schedules of agent classes plus any other action taken by the world, such as incrementing time or calculating summary statistics
-    	"""
-    	self.increment_time()
+        """ This method is supposed to be overridden.
+        It should include the schedules of agent classes plus any other action taken by the world, such as incrementing time or calculating summary statistics
+        """
+        self.increment_time()
 
-
-
-    def create_agents(self,agent_type,n,pos=None, **kwargs):
+    def create_agents(self, agent_type, n, pos=None, **kwargs):
         """ Create 'n' 'agent_type' agents.
 
         Args:
@@ -73,14 +72,14 @@ class World(object):
         Returns:
             None.
         """
-        if pos==None:
+        if pos == None:
             for i in range(n):
-                random_pos=(np.random.rand()*self.topology.x_max,np.random.rand()*self.topology.y_max)
-                agent_type(position=random_pos,**kwargs)
+                random_pos = (np.random.rand() * self.topology.x_max,
+                              np.random.rand() * self.topology.y_max)
+                agent_type(position=random_pos, **kwargs)
         else:
             for i in range(n):
-                agent_type(position=pos[i],**kwargs)
-
+                agent_type(position=pos[i], **kwargs)
 
     def ask(self, agents, methodname, *args, **kwargs):
         """ Make 'agents' execute the 'methodname'
@@ -96,9 +95,7 @@ class World(object):
         """
         f = methodcaller(methodname, *args, **kwargs)
         for agent in agents:
-                f(agent)
-
-
+            f(agent)
 
     def increment_time(self):
         """ Increment step, day, month and year counters.
@@ -106,26 +103,25 @@ class World(object):
         Returns:
             None.
         """
-        self.step+=1
-        if self.step>self.steps_in_day:
-            self.step=1
+        self.step += 1
+        if self.step > self.steps_in_day:
+            self.step = 1
             self.increment_day()
-        if self.day>30:
-            self.day=1
+        if self.day > 30:
+            self.day = 1
             self.increment_month()
-        if self.month>12:
-            self.month=1
+        if self.month > 12:
+            self.month = 1
             self.increment_year()
 
     def increment_day(self):
-        self.day+=1
+        self.day += 1
 
     def increment_month(self):
-        self.month+=1
+        self.month += 1
 
     def increment_year(self):
-        self.year+=1
+        self.year += 1
 
-
-    def steps_from_years(self,n_years):
-        return n_years*12*30*self.steps_in_day
+    def steps_from_years(self, n_years):
+        return n_years * 12 * 30 * self.steps_in_day
